c399b7e761b056c093858220d4d9a4414b3b6948
Gogi <Gogi22@users.noreply.github.com>
2021-09-30 17:55:55 +0400

    added CalestialObject class

---
+++ StarChart/Models/CalestialObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StarChart.Models
+{
+    public class CalestialObject
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Nam { get; set; }
+        public int? OrientedObjectId { get; set; }
+        [NotMapped]
+        public List<CalestialObject> Satellites { get; set; }
+        public TimeSpan timeSpan { get; set; }
+
+    }
+}
